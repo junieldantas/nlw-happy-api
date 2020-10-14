0d0ffba0f1f024554d406a813debd15f08e82d1a
Juniel Dantas <juniel.dantas@gmail.com>
2020-10-14 00:39:19 -0300

    Start commit

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphanagesController from './controllers/OrphanagesController';
+
+const route = Router();
+const upload = multer(uploadConfig);
+
+route.get('/orphanages', OrphanagesController.index);
+route.get('/orphanages/:id', OrphanagesController.show);
+route.post('/orphanages', upload.array('images'),OrphanagesController.create);
+
+export default route;
