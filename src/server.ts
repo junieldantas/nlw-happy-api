c21dadc06edb4546d7e75eced5b776ec48f284ea
Juniel Dantas <juniel.dantas@gmail.com>
2020-12-07 03:30:42 -0300

    AJS

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import errorHandler from './errors/handler';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://192.168.0.5:3000'
 }));
 app.use(express.json());
 app.use(routes);
